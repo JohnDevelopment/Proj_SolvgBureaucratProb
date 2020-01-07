b982e441b831c8f0501ca0d07c3852730f56f89e
JohnDevelopment <ionpetru.virlan@mail.ru>
2020-01-07 13:56:16 +0200

    Add solvingBureaucratProb Project
    First loading solvingBureaucratProb Project

---
+++ solvingBureaucratProb/src/main/java/com/project/virlanion/solvingBureaucratProb/SolvingBureaucratProbApplication.java
@@ -0,0 +1,13 @@
+package com.project.virlanion.solvingBureaucratProb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvingBureaucratProbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvingBureaucratProbApplication.class, args);
+	}
+
+}
